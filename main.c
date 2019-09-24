658cbe5388b55119e3d30dab8cbbb3cebfa189b9
tomdrc1 <tomdrc4@gmail.com>
2019-09-24 17:53:26 +0300

    Removed the experemental scaleup to see if it worked on Linux. It did. Will now try to connect the buttons and switeches to the raspbeery

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ void setupMachine(spaceInvaderMachine* machine)
 
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK);
 
-	SDL_CreateWindowAndRenderer(SCREEN_HEIGHT, SCREEN_WIDTH, 0, &screen, &renderer);
+	SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &screen, &renderer);
 	SDL_ShowCursor(SDL_DISABLE);
 
 	machine->screen = screen;
